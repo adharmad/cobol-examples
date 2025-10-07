dc1d3707c4d2d24f1b17ff10d185051d36a6fce1
Amol Dharmadhikari <adharmad@ionate.io>
2025-10-07 10:23:33 -0700

    Adding sample for cobol comp-4 redefine

---
+++ sources/test_redef_comp4.cbl
@@ -0,0 +1,20 @@
+      * Test COMP-4
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. TEST-COMP4.
+
+       DATA DIVISION.
+       WORKING-STORAGE SECTION.
+
+       01  OBJ1.
+          05  FILLER              PIC  X(01).
+          05  NUM-X               PIC  X(01).
+       01 NUM1 REDEFINES OBJ1 PIC S9(04) BINARY.
+
+       PROCEDURE DIVISION.
+
+      *MOVE SPACES to STR.
+      *    INITIALIZE OBJ1.
+           DISPLAY ":"OBJ1":".
+           DISPLAY ":"NUM1":".
+
+       STOP RUN.
